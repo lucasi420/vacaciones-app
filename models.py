f4afaa1ef098bea9a1e667efa9bb396374fc7fe4
lucasi420 <lucasismaelf@yahoo.com>
2025-09-28 19:46:20 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from datetime import date
 
 # Creamos la base de datos
 db = SQLAlchemy()
@@ -10,11 +11,14 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)  # True = superusuario
-from datetime import date
+    
+    # Columna para almacenar el color único de cada usuario en el calendario
+    color = db.Column(db.String(7), nullable=False, default="#333333") 
 
 class Vacation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    date = db.Column(db.String(10), nullable=False)  # formato 'YYYY-MM-DD'
+    date = db.Column(db.String(10), nullable=False)  # formato 'YYYY-MM-DD' 
 
+    # Define la relación con el usuario
     user = db.relationship('User', backref=db.backref('vacations', lazy=True))
